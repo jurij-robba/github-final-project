13a077cb25ccef5bbcd59f4065c3c87ab2ec73cb
Jurij Robba <30544241+jurij-robba@users.noreply.github.com>
2023-04-21 08:15:11 +0200

    added author
    not really an author, but it is what it is.

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # jurij-robba
    # Input:
    # p, principal amount
    # t, time period in years
